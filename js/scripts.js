46c49b3e93579b97beddd23f3e16e9e26003d860
Sandra Tena <sandratena06@gmail.com>
2022-06-03 09:07:28 -0700

    Add all questions to form function

--- js/scripts.js
+++ js/scripts.js
@@ -2,5 +2,10 @@ $(document).ready(function(){
   $("form#languageSuggester").submit(function(event){
     event.preventDefault();
     const question1 = $("select#question1").val();
+    const question2 = $("select#question2").val();
+    const question3 = $("select#question3").val();
+    const question4 = $("select#question4").val():
+    const question5 = $("select#question5").val();
+    
   })
 })

3ef3c969bc755edb9e869469ca79031367eaae43
Sandra Tena <sandratena06@gmail.com>
2022-06-03 10:43:29 -0700

    Add branching to form function and add hide/show toggle function to scripts.js and index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,43 @@
 $(document).ready(function(){
   $("form#languageSuggester").submit(function(event){
-    event.preventDefault();
     const question1 = $("select#question1").val();
     const question2 = $("select#question2").val();
     const question3 = $("select#question3").val();
-    const question4 = $("select#question4").val():
+    const question4 = $("select#question4").val();
     const question5 = $("select#question5").val();
     
+    if (question1 === "no" && question2 === "no" && question3 === "no" && question4 === "no" && question5 === "no" || question1 === "no" && question2 === "no" && question3 === "no" && question4 === "no"&& question5 === "yes") {
+      $("#javascriptResult").show();
+    } else if (question1 === "yes" && question2 === "no" && question3 === "no" && question4 === "no" && question5 === "no" || question1 === "yes" && question2 === "no" && question3 === "no" && question4 === "no" && question5 === "yes") {
+      $("#cSharpResult").show();
+    } else if (question1 === "yes" && question2 === "yes" && question3 === "yes" && question4 === "no" && question5 === "no" || question1 === "yes" && question2 === "yes" && question3 === "yes" && question4 === "no" && question5 === "yes") {
+      $("#reactResult").show();
+    } else if (question1 === "yes" && question2 === "yes" && question3 === "no" && question4 === "yes" && question5 === "no" || question1 === "yes" && question2 === "yes" && question3 === "no" && question4 === "yes" && question5 === "yes") {
+      $("#rubyResult").show();
+    } else {
+      $("#epicodusResult").show();
+    }
+    
+    event.preventDefault();
+  })
+  $(".clickable").click(function(){
+    $("#javascriptResult-show").toggle();
+    $("#result-hidden").toggle();
+  })
+  $(".clickable").click(function(){
+    $("#cSharpResult-show").toggle();
+    $("#result-hidden").toggle();
+  })
+  $(".clickable").click(function(){
+    $("#reactResult-show").toggle();
+    $("#result-hidden").toggle();
+  })
+  $(".clickable").click(function(){
+    $("#rubyResult-show").toggle();
+    $("#result-hidden").toggle();
+  })
+  $(".clickable").click(function(){
+    $("#epicodusResult-show").toggle();
+    $("#result-hidden").toggle();
   })
 })
